161d5bae64d0394ff4ede973574e5f80c0fbaa19
jayden vause <jaydenvausemailbox@gmail.com>
2023-03-09 17:35:37 +1100

    fixed ui for auth forms

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -22,40 +22,42 @@ const submit = () => {
 <template>
     <MainTemplate>
         <Head title="Forgot Password" />
-        <div class="bg-white p-4">
-            <h2 class="mb-4 text-2xl">Forgot Password?</h2>
-                <div class="mb-4 text-sm text-gray-600">
-                    Forgot your password? No problem. Just let us know your email address and we will email you a password reset
-                    link that will allow you to choose a new one.
-                </div>
-
-                <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-                    {{ status }}
-                </div>
-
-                <form @submit.prevent="submit">
-                    <div>
-                        <InputLabel for="email" value="Email" />
-
-                        <TextInput
-                            id="email"
-                            type="email"
-                            class="mt-1 block w-full"
-                            v-model="form.email"
-                            required
-                            autofocus
-                            autocomplete="username"
-                        />
-
-                        <InputError class="mt-2" :message="form.errors.email" />
+        <div class="max-w-[1200px] m-auto pt-3">
+            <div class="bg-white p-4">
+                <h2 class="mb-4 text-2xl">Forgot Password?</h2>
+                    <div class="mb-4 text-sm text-gray-600">
+                        Forgot your password? No problem. Just let us know your email address and we will email you a password reset
+                        link that will allow you to choose a new one.
                     </div>
 
-                    <div class="flex items-center justify-end mt-4">
-                        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                            Email Password Reset Link
-                        </PrimaryButton>
+                    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                        {{ status }}
                     </div>
-                </form>
+
+                    <form @submit.prevent="submit">
+                        <div>
+                            <InputLabel for="email" value="Email" />
+
+                            <TextInput
+                                id="email"
+                                type="email"
+                                class="mt-1 block w-full"
+                                v-model="form.email"
+                                required
+                                autofocus
+                                autocomplete="username"
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.email" />
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                Email Password Reset Link
+                            </PrimaryButton>
+                        </div>
+                    </form>
+            </div>
         </div>
     </MainTemplate>
 </template>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -32,61 +32,63 @@ const submit = () => {
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
         </div>
-        <div class="bg-white p-4">
-            <h2 class="mb-4 text-2xl">Login</h2>
-            <form @submit.prevent="submit">
-                <div>
-                    <InputLabel for="email" value="Email" />
+        <div class="max-w-[1200px] m-auto pt-3">
+            <div class="bg-white p-4">
+                <h2 class="mb-4 text-2xl">Login</h2>
+                <form @submit.prevent="submit">
+                    <div>
+                        <InputLabel for="email" value="Email" />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        class="mt-1 block w-full"
-                        v-model="form.email"
-                        required
-                        autofocus
-                        autocomplete="username"
-                    />
+                        <TextInput
+                            id="email"
+                            type="email"
+                            class="mt-1 block w-full"
+                            v-model="form.email"
+                            required
+                            autofocus
+                            autocomplete="username"
+                        />
 
-                    <InputError class="mt-2" :message="form.errors.email" />
-                </div>
+                        <InputError class="mt-2" :message="form.errors.email" />
+                    </div>
 
-                <div class="mt-4">
-                    <InputLabel for="password" value="Password" />
+                    <div class="mt-4">
+                        <InputLabel for="password" value="Password" />
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        class="mt-1 block w-full"
-                        v-model="form.password"
-                        required
-                        autocomplete="current-password"
-                    />
+                        <TextInput
+                            id="password"
+                            type="password"
+                            class="mt-1 block w-full"
+                            v-model="form.password"
+                            required
+                            autocomplete="current-password"
+                        />
 
-                    <InputError class="mt-2" :message="form.errors.password" />
-                </div>
+                        <InputError class="mt-2" :message="form.errors.password" />
+                    </div>
 
-                <div class="block mt-4">
-                    <label class="flex items-center">
-                        <Checkbox name="remember" v-model:checked="form.remember" />
-                        <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
+                    <div class="block mt-4">
+                        <label class="flex items-center">
+                            <Checkbox name="remember" v-model:checked="form.remember" />
+                            <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                        </label>
+                    </div>
 
-                <div class="flex items-center justify-end mt-4">
-                    <Link
-                        v-if="canResetPassword"
-                        :href="route('password.request')"
-                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Forgot your password?
-                    </Link>
+                    <div class="flex items-center justify-end mt-4">
+                        <Link
+                            v-if="canResetPassword"
+                            :href="route('password.request')"
+                            class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Forgot your password?
+                        </Link>
 
-                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
+                        <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Log in
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
         </div>
     </MainTemplate>
 </template>
